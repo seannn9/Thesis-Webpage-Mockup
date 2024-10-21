1af60c63ca1fef471d78e81b971b68b6158a2261
seannn9 <naeskirlu9@gmail.com>
2024-10-21 14:46:49 +0800

    added new page called parameters

--- src/pages/Parameters.jsx
+++ src/pages/Parameters.jsx
@@ -1,5 +1,11 @@
 import { Link } from "react-router-dom";
 import React from "react";
+import "../styles/Parameters.css";
+import humidityImg from "../images/humidity.png";
+import tempImg from "../images/temperatures.png";
+import lightImg from "../images/bulb.png";
+import soilMoistImg from "../images/meter.png";
+import Card from "../components/Card"; // Card component to avoid repeating code
 
 function Parameters() {
     return (
@@ -15,6 +21,47 @@ function Parameters() {
                 <Link className="loginNav" to="/login">
                     Login
                 </Link>
+            </div>
+            <div className="adjustTitle">
+                <h1>Adjust Parameters</h1>
+            </div>
+            <div className="parametersCards">
+                {/* <div className="card humidity">
+                    <p>HUMIDITY</p>
+                    <img src={humidityImg} alt="Humidity Icon" />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <form action="">
+                        <label htmlFor="">Input a Value</label>
+                        <br />
+                        <input
+                            type="text"
+                            name="humidityVal"
+                            className="humidityVal"
+                            placeholder="Value"
+                        />
+                    </form>
+                </div> */}
+                <Card
+                    name="humidityVal"
+                    inputClassName="inputHumidityVal"
+                    className="card humidity"
+                    src={humidityImg}
+                />
+                <div className="card temperature">
+                    <p>TEMPERATURE</p>
+                    <img src={tempImg} alt="Temperature Icon" />
+                </div>
+                <div className="card light">
+                    <p>LIGHT</p>
+                    <img src={lightImg} alt="Light Bulb Icon" />
+                </div>
+                <div className="card soilMoisture">
+                    <p>SOIL MOISTURE</p>
+                    <img src={soilMoistImg} alt="Soil Moisture Icon" />
+                </div>
             </div>
         </>
     );
