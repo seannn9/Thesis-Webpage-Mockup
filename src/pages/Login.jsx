19ca098d3590478c7650a9c46da48dae75222075
seannn9 <naeskirlu9@gmail.com>
2024-10-23 15:41:59 +0800

    Finished styling and strcture of login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,61 @@
-import { Link } from "react-router-dom";
-import { HashLink } from "react-router-hash-link";
 import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
+import "../styles/Login.css";
 
 function Login() {
     return (
         <>
             <Navbar activeLogin="active" />
-            <div className="mainContainer"></div>
+            <div className="mainContainer">
+                <div className="formContainer">
+                    <form action="" method="POST">
+                        <legend>Sign Up</legend>
+                        <label>Email:</label>
+                        <br />
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            required
+                        />
+                        <br />
+                        <label>Password:</label>
+                        <br />
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            required
+                        />
+                        <br />
+                        <div className="acceptBox">
+                            <input
+                                type="checkbox"
+                                id="acceptTerms"
+                                name="acceptTerms"
+                                value="Accept"
+                                required
+                            />
+                            <label htmlFor="acceptTerms" className="terms">
+                                I accpet all terms & condtions
+                            </label>
+                        </div>
+                        <button
+                            type="submit"
+                            value="Login"
+                            className="loginBtn"
+                        >
+                            Register
+                        </button>
+                        <br />
+                        <br />
+                        <p>
+                            Already have an account?&nbsp;
+                            <Link to="/">Login</Link>
+                        </p>
+                    </form>
+                </div>
+            </div>
         </>
     );
 }
